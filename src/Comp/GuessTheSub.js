058d16fed8f9ad772b599a2bec7abba3f93cc529
renan <renanbazinin2@gmail.com>
2022-06-17 16:59:52 +0300

    text

--- src/Comp/GuessTheSub.js
+++ src/Comp/GuessTheSub.js
@@ -75,7 +75,7 @@ export default function GuessTheSub() {
         {!score.waiting?subChose:""}
         <br/><br/><br/>
         {data!==null?
-        <div>
+        <div style={{"marginLeft":"10%", "marginRight":"10%","borderStyle":"solid"}}>
         {data.data.title}
         <br/>
 
