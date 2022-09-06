451d9d30bb430689e45bc0a64a69cb6cf41cf4b4
renan <renanbazinin2@gmail.com>
2022-09-06 14:44:45 +0300

    add C section

--- src/Comp/Math/LeanC.js
+++ src/Comp/Math/LeanC.js
@@ -1,6 +1,9 @@
-import React from 'react'
 
+import React, { useState, useEffect } from 'react';
 export default function LeanC() {
+
+  const [url,setUrl] = useState("");
+
   const handleNav = (e)=>{
    
 
@@ -18,17 +21,32 @@ export default function LeanC() {
      btc.className='a-pressed';
     
   } 
- 
+
+  setUrl(e.target.id)
+
+
 }
 
   return (
     <div>
       <br/>
             <div class="topnav-heb" style={{"margin":"0"}} >
-                  <a onClick={handleNav}>כלום1</a>
-                  <a onClick={handleNav}>כלום 2 </a>
+                  <a onClick={(e)=>{handleNav(e)}} id='https://drive.google.com/file/d/10dZjX1qrRoV9j9MxNvd4RjpEqDCGeFqu/preview'>פתח דבר ויצוג בסיסיים שונים</a>
+                  <a > לא אעשה הסבר על יסודות התכנות</a>
+                  <a onClick={handleNav}  id='https://drive.google.com/file/d/10gEDOTcMjnQ_-cta4VJWXz580_8ppwmq/preview' >מגבלת הטיפוס int וייצוג מספר שלילי</a>
+                  <a onClick={handleNav}  id='https://drive.google.com/file/d/10mE18Qa_SbnFndnToF_Agb9rWVggZ2Wb/preview'>  כתובות ומצביעים </a>
               </div>
 
+      <div>
+        {url.length>5?
+      <iframe
+        frameborder="0"
+        scrolling="no"
+        src={url}>
+
+
+        </iframe>:""}
+      </div>
     </div>
   )
 }
