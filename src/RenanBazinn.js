a75e3f4a39edfa02d70fdb96fa34c8ecdfe3307b
renan <renanbazinin2@gmail.com>
2022-10-01 20:41:53 +0300

    just text

--- src/RenanBazinn.js
+++ src/RenanBazinn.js
@@ -55,7 +55,7 @@ export default function RenanBazinn() {
                 <ul>
       
                   <li>C# from high school (97 score in computer science)</li>
-                  <li>JAVA from Open University (82 score)</li>
+                  <li>JAVA from Open University </li>
                   <ul><li>OOP, recursion and how to calculate time complexity </li></ul>
                   <li>Python from project in 5 Point the software engineering (100 score)</li>
                   </ul>
